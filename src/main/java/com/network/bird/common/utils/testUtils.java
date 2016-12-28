be82593dac6e8881528091db95266cd2bf90ae8f
yong3.li <1161286891@qq.com>
2016-12-28 17:50:30 +0800

    框架搭建中，雏形暂定

--- src/main/java/com/network/bird/common/utils/testUtils.java
+++ src/main/java/com/network/bird/common/utils/testUtils.java
@@ -1,7 +1,7 @@
 package com.network.bird.common.utils;
 
 /**
- * Created by DHC on 2016/12/28.
+ * Created by zhoupeng on 2016/12/28.
  */
 public class testUtils {
 }
--- src/main/java/com/network/bird/mvc/dto/testDto.java
+++ src/main/java/com/network/bird/mvc/dto/testDto.java
@@ -3,7 +3,7 @@ package com.network.bird.mvc.dto;
 /**
  * 接口 提供类
  *
- * Created by DHC on 2016/12/28.
+ * Created by zhoupeng on 2016/12/28.
  */
 public class testDto {
 }
